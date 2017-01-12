2b40d9bd09fe4876fcb511199e001cddc5e42f4d
InspectorGadget <igadget28@gmail.com>
2017-01-12 22:15:45 +0800

    Finished "ClearMyChat"

---
+++ src/RTG/ClearMyChat/Main.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * All rights reserved RTGNetworkkk
+ * This plugin was not stolen and i aquire equal rights to sue!
+*/
+
+namespace RTG\ClearMyChat;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat as TF;
+
+class Main extends PluginBase {
+	
+	public function onEnable() {
+	}
+	
+	public function onCommand(CommandSender $sender, Command $cmd, $label, array $param) {
+		switch(strtolower($cmd->getName())) {
+			
+			case "clear":
+				$sender->sendMessage("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nn\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+				$sender->sendMessage(TF::GREEN . "You chat has been Cleared!");
+				return true;
+			break;
+			
+		}
+	}
+	
+	public function onDisable() {
+	}
+
+}
